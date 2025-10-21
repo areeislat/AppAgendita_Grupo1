d8bea5c55786f6fb2e0b3419b07827b8a33998cc
Areliz Isla <are.isla@duocuc.cl>
2025-10-21 18:23:14 -0300

    Modificacion TAREAS

--- app/src/main/java/com/example/appagendita_grupo1/ui/screens/AddTaskScreen.kt
+++ app/src/main/java/com/example/appagendita_grupo1/ui/screens/AddTaskScreen.kt
@@ -17,7 +17,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.AccountCircle
@@ -135,7 +137,8 @@ fun AddTaskScreen(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding)
-                .padding(16.dp),
+                .padding(16.dp)
+                .verticalScroll(rememberScrollState()),
             verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
             CardSection(title = "Detalles de la tarea") {
